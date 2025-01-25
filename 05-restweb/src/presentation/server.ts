8fbb79e256145e7be3af71274c90aead7126e5f1
EMEBE <81788252+MilthonB@users.noreply.github.com>
2025-01-25 14:41:27 -0600

    scrips updated

--- 05-restweb/src/presentation/server.ts
+++ 05-restweb/src/presentation/server.ts
@@ -1,18 +1,33 @@
 import express  from 'express';
 
 
+interface Options {
+
+    port: number;
+
+}
+
+
 
 export class Server{
 
 
     private app = express();
 
+    private readonly port : number; 
+
+
+    constructor(opciones: Options){
+        const {port} = opciones;
+        this.port =  port;
+    }
+
  
     
     
     async start() {
         this.app.use( express.static('public') );
-        this.app.listen(3000, ()=>{
+        this.app.listen(this.port, ()=>{
             console.log('Server runngin 3000')
         })
     }
